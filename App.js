bf253a24c6a0679e0f329c6c8d44ec8edddfcf83
Jeff Webb <webbwear@icloud.com>
2022-09-09 15:07:29 -0400

    Fetch data from rails app

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>PrivateDriver.io</Text>
+export default class App extends React.Component {
+  state = {
+    data: {'activity': 'Loading...'}
+  }
 
-    <TouchableOpacity
-        onPress={() => alert('Hello, Data!')}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Get Data</Text>
-      </TouchableOpacity>
+  getJsonData = () => {
+    fetch('http://localhost:3000/vehicles.json',
+    {method: "GET"}).then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      this.setState({
+        data: responseJson
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+  }
 
-      <StatusBar style="auto" />
-    </View>
-  );
+  componentDidMount = () => {
+    this.getJsonData()
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+      
+        <Text style={styles.text}>PrivateDriver.io</Text>
+          <View>
+            <Text style={styles.text}>Vehicle</Text>
+            <Text style={{margin:10, fontSize:16}}>{'Make ' + this.state.data['make']}</Text>
+            <Text style={{margin:10, fontSize:16}}>{'Model ' + this.state.data['model']}</Text>
+          </View>
+        <TouchableOpacity
+          onPress={(data) => alert((this.state.data['make']))}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Get Data</Text>
+        </TouchableOpacity>
+  
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
