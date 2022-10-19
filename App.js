f4de8291b28a2444a78435b64a049b802a0bfadb
Jeff Webb <webbwear@icloud.com>
2022-10-19 16:50:59 -0400

    wip, add formik for forms

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 // screens
 import Login from './screens/Login';
@@ -9,11 +8,3 @@ export default function App() {
   return <Login />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+// Formik
+import { Formik } from 'formik';
 
-import { StyledContainer, InnerContainer, PageLogo, PageTitle } from './../components/styles';
+
+import { 
+    StyledContainer, 
+    InnerContainer, 
+    PageLogo, 
+    PageTitle, 
+    SubTitle, 
+    StyledFormArea, 
+} from './../components/styles';
+
+import { View } from 'react-native';
 
 const Login = () => {
     return (
         <StyledContainer>
-            <StatusBar style="dark" />
+            <StatusBar style="light" />
             <InnerContainer>
                 <PageLogo resizeMode="contain" source={require('./../assets/privatedriver-logo-app.png')} />
                 {/* <PageTitle>A Driver Driven App</PageTitle> */}
+                <SubTitle>Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+
+                </Formik>
             </InnerContainer>
         </StyledContainer>
     );
 }
+
+// const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
 
 export default Login;
