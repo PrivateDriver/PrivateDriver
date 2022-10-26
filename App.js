bc76c105e99948546d5f549f4a01b0862441b1b7
Jeff Webb <webbwear@icloud.com>
2022-10-26 09:53:31 -0400

    wip

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Tabs from './navigation/tabs';
 
 // screens
-import Login from './screens/login/Login';
+// import Login from './screens/login/Login';
 import Events from './screens/events/Events';
 import Drivers from './screens/drivers/Drivers';
 import Clients from './screens/clients/Clients';
@@ -14,8 +15,9 @@ export default function App() {
   return (
     // <Login />
     <NavigationContainer>  
+    {/* <Tabs /> */}
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen name="Events" component={Events} />
         <Stack.Screen name="Drivers" component={Drivers} />
         <Stack.Screen name="Clients" component={Clients} />
