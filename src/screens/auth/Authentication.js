f6c7dedac235aae7dca239c9e7b2266e56481784
Jeff Webb <webbwear@icloud.com>
2023-03-14 12:08:07 -0400

    Module for auth asyn-storage and logic for login rolls

---
+++ src/screens/auth/Authentication.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+const ASuthentication = () => {
+  return (
+    <View>
+      <Text>authentication</Text>
+    </View>
+  )
+}
+
+export default authentication
+
+const styles = StyleSheet.create({})
