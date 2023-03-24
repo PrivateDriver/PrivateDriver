aaf4503d9263f162ac48fe07ec987d6b12328a33
Jeff Webb <webbwear@icloud.com>
2023-03-24 15:49:14 -0400

    Api to backend pulls data and displays Flatlist on eventScreen

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,188 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { Button, View, Text, SafeAreaView, FlatList, StyleSheet, StatusBar } from 'react-native'
 
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import axios from 'axios';
+import axios from 'axios'
 
-//formik
-import { Formik } from 'formik'
-//icons
+const EventScreen = ({ navigation }) => {
+  const [events, setEvents] = useState([])
 
-import { Octicons, Ionicons } from '@expo/vector-icons';
+  useEffect(() => {
+    async function loadData() {
+      const response = await axios.get('https://limo-app-server.loca.lt/events')
+      console.log(response.data)
+      setEvents(response.data)
+    }
+    loadData()
+  }, [])
 
+  // Flatlist component
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  )
 
-import {
-  StyledContainer,
-  InnerContainer,
-  PageLogo,
-  SubTitle,
-  StyledFormArea,
-  LeftIcon,
-  StyledInputLabel,
-  StyledTextInput,
-  RightIcon,
-  StyledButton,
-  ButtonText,
-  Colors,
-  MsgBox,
-  Line,
-  ExtraView,
-  ExtraText,
-  TextLink,
-  TextLinkContent,
-} from '../../components/styles';
-import { View } from 'react-native';
+  // Refactor Buttons to BottomTabNavigator
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={events}
+        renderItem={({ item: event }) => <Item title={event.mileage} />}
+        keyExtractor={item => item.id}
+      />
 
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
+        <View>
+          <Text>There are {events.length} events</Text>
+        </View>
 
-const JeffSubmit = () => {
-  console.log('Hi')
+        <SafeAreaView>
+          <Button title="Drivers" onPress={() => navigation.navigate('Drivers')} />
+
+          <Button title="Clients" onPress={() => navigation.navigate('Clients')} />
+
+          <Button title="Vehicles" onPress={() => navigation.navigate('Vehicles')} />
+        </SafeAreaView>
+      </View>
+    </SafeAreaView>
+  )
 }
 
+//// Refactor Styles for Flatlist
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#8e99a3',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 22,
+  },
+})
+////
 
-
-//Colors
-const { brand, tertiary } = Colors;
-
-const LoginScreen = ({ navigation }) => {
-  const [hidePassword, setHidePassword] = useState(true);
-
-  return (
-    <StyledContainer>
-      <StatusBar style='light-dark' />
-      <InnerContainer>
-        <PageLogo
-          resizeMode='contain'
-          source={require('../../assets/icons/privatedriver-logo-app.png')}
-        />
-        <SubTitle>Account Login</SubTitle>
-        <Formik
-          initialValues={{ email: 'me@email.com', password: 'password' }}
-          onSubmit={async (values) => {
-            try {
-              const payload = {
-                user: {
-                  email: values.email,
-                  password: values.password,
-                },
-              };
-              console.log(payload);
-
-              // Login Auth
-              const response = await axios.post(
-                'https://limo-app-server.loca.lt/api/auth',
-                payload
-              );
-              if (response.status === 401) {
-                console.log('Invalid credentials');
-              }
-              if (response.status === 200) {
-                console.log('Logged in successfully');
-
-                // Actual auth work here....
-                //
-                // LocalStorage
-                // The authorization header is what we need to
-                //   save and send back in all future requests
-                // console.log(response.headers.authorization)
-                // localStorage.setItem('auth', response.headers.authorization)
-
-                // Set a default for axios so all future requests
-                // automatically get that header
-                // If we use this code we don't need the localStorage business
-                // since axios will automatically send the header for us.
-                axios.defaults.headers.common['Authorization'] =
-                  response.headers.authorization;
-
-                // When you logout, send the DELETE /api/auth request and
-                // then remove the auth header from axios
-                // axios.defaults.headers.common['Authorization'] = null
-
-                // Later on:
-                //   const auth = localStorage.getItem('auth')
-                //   const response = await axios.get('https://limo-app-server.loca.lt', { headers: { Authorization: auth } })
-
-                // Now we can navigate to the home screen
-                navigation.navigate('Events');
-              }
-            } catch (error) {
-              //ERROR Logic here
-            }
-          }}
-        >
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <StyledFormArea>
-              <MyTextInput
-                label='Email Address'
-                icon='mail'
-                placeholder='your@email.com'
-
-                placeholderTextColor={tertiary}
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                value={values.email}
-                keyboardType='email-address'
-              />
-
-              <MyTextInput
-                label='Password'
-                icon='lock'
-                placeholder='* * * * * * * * * *'
-
-                placeholderTextColor={tertiary}
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.password}
-                secureTextEntry={hidePassword}
-                isPassword={true}
-                hidePassword={hidePassword}
-                setHidePassword={setHidePassword}
-              />
-              <MsgBox>...</MsgBox>
-              <StyledButton onPress={handleSubmit}>
-                <ButtonText>LOGIN</ButtonText>
-              </StyledButton>
-              <Line />
-              <ExtraView>
-                <ExtraText>Add another account to member profile?</ExtraText>
-                <TextLink>
-                  <TextLinkContent> Signup</TextLinkContent>
-                </TextLink>
-              </ExtraView>
-            </StyledFormArea>
-          )}
-        </Formik>
-      </InnerContainer>
-    </StyledContainer>
-  );
-};
-
-const MyTextInput = ({
-  label,
-  icon,
-  isPassword,
-  hidePassword,
-  setHidePassword,
-  ...props
-}) => {
-
-  return (
-    <View>
-      <LeftIcon>
-        <Octicons name={icon} size={30} color={brand} />
-      </LeftIcon>
-      <StyledInputLabel>{label}</StyledInputLabel>
-      <StyledTextInput {...props} />
-      {isPassword && (
-        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-          <Ionicons
-            name={hidePassword ? 'md-eye-off' : 'md-eye'}
-            size={30}
-            color={brand}
-          />
-        </RightIcon>
-      )}
-    </View>
-  );
-};
-
-
-export default LoginScreen;
+export default EventScreen
