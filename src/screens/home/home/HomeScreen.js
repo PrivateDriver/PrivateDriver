0e506454c84be6b6cae5d9e8c5185a9cd750d608
Jeff Webb <webbwear@icloud.com>
2023-06-29 17:06:14 -0400

    wip

--- src/screens/home/home/HomeScreen.js
+++ src/screens/home/home/HomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Button, View, Text, SafeAreaView } from 'react-native'
+import { StyledButton, ButtonText } from '../../../components/styles'
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text> HOME SCREEN</Text>
-      <SafeAreaView>
-        <Button title="Driver" onPress={() => navigation.navigate('Driver')} />
-      </SafeAreaView>
+      <StyledButton
+        onPress={() => {
+          navigation.navigate('Vehicle')
+        }}
+      >
+        <ButtonText>Check Out Vehicle</ButtonText>
+      </StyledButton>
+      <StyledButton>
+        <ButtonText>Check In Vehicle</ButtonText>
+      </StyledButton>
     </View>
   )
 }
--- src/screens/index.js
+++ src/screens/index.js
@@ -11,3 +11,6 @@ export { default as ForgotPassword } from './auth/ForgotPassword'
 export { default as ClientProfile } from './home/clients/ClientProfile'
 export { default as DriverProfile } from './home/drivers/DriverProfile'
 export { default as VehicleProfile } from './home/vehicles/VehicleProfile'
+// Checking Vehicle Screens
+// export { default as CheckOut } from './home/vehicles/CheckOut'
+// export { default as CheckIn } from './home/vehicles/CheckIn'
